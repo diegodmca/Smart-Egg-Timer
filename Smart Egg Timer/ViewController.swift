7c1a075d57962735f3c71ce5836d115a34949022
Diego Carvalho <diegodmca@outlook.com>
2022-05-28 15:35:06 -0300

    Initial Commit

---
+++ Smart Egg Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart Egg Timer
+//
+//  Created by Diego Carvalho on 28/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
